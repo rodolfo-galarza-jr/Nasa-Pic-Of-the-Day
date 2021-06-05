a3aaef6b0fc01d3d5f1f31c416c034122c647c16
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2021-06-04 22:09:14 -0500

    Initial Commit

---
+++ Nasa Pic Of the Day/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nasa Pic Of the Day
+//
+//  Created by Rodolfo Galarza Jr on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nasa Pic Of the Day/Nasa_Pic_Of_the_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nasa_Pic_Of_the_DayApp.swift
+//  Nasa Pic Of the Day
+//
+//  Created by Rodolfo Galarza Jr on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_Pic_Of_the_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
