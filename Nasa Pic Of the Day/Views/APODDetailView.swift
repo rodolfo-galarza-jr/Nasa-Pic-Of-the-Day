99bb53feddfe97f79ccb38ebe3964f27c6a9f678
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2021-06-12 15:46:35 -0500

    Add additional API calls at end of list

---
+++ Nasa Pic Of the Day/Views/APODDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  APODDetailView.swift
+//  Nasa Pic Of the Day
+//
+//  Created by Rodolfo Galarza Jr on 6/12/21.
+//
+
+import SwiftUI
+
+struct APODDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct APODDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        APODDetailView()
+    }
+}
